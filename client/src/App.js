6f8277e0ab0b4435c763f2b8876488845ca73091
Roberto <mayanhavoc@gmail.com>
2021-11-18 12:19:49 -0800

    Here it goes, hope this helps! So sorry!

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,6 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React from 'react'
+import Paint from './components/Paint'
 
-const App = () => {
-  useLayoutEffect(() => {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = 'green';
-    ctx.fillRect(10,10, 150, 100);
-  })
-
-
-  return (
-    <canvas 
-    id="canvas" 
-    style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}} 
-    width={window.innerWidth} 
-    height={window.innerHeight}
-    >
-      Canvas
-    </canvas>
-  )
+export default function App() {
+  return <Paint />
 }
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { CanvasProvider } from './CanvasContext';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <CanvasProvider>
       <App />
-    </CanvasProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
