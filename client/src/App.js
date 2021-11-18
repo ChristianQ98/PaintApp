8a23d3681ccb6b30a2c1d3113f3dffd4ff71ca1a
Roberto <mayanhavoc@gmail.com>
2021-11-18 11:46:50 -0800

    canvas

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import { Canvas } from './Canvas'
-import { ClearCanvasButton } from './ClearCanvasButton';
+import React, { useLayoutEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  useLayoutEffect(() => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10,10, 150, 100);
+  })
+
+
   return (
-    <>
-      <Canvas/>
-      <ClearCanvasButton/>
-    </>
-  );
+    <canvas 
+    id="canvas" 
+    style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}} 
+    width={window.innerWidth} 
+    height={window.innerHeight}
+    >
+      Canvas
+    </canvas>
+  )
 }
 
 export default App;
